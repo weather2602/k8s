bf2dbbcc4e9555ef88c4507504abccfece079e9c
Nishant <nishant260296@gmail.com>
2025-11-10 11:57:21 +0100

    fix: provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,17 @@
-# terraform {
-#   required_version = ">= 1.6.0"
-#   required_providers {
-#     google = {
-#       source  = "hashicorp/google"
-#       version = "~> 6.0"
-#     }
-#   }
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
 
-#   backend "gcs" {}
-# }
+  backend "gcs" {}
+}
 
-# provider "google" {
-#   project = var.project_id
-#   region  = var.region
-#   zone    = var.zone
-# }
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
