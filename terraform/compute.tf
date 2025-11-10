2d7dc3f4b04804f84abd9b6bdda0874071a10b42
Nishant <nishant260296@gmail.com>
2025-11-10 11:55:13 +0100

    rm: comment out  resources.

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,37 +1,37 @@
-resource "google_compute_instance" "controlplane" {
-  name         = "k8s-controlplane"
-  machine_type = "e2-medium"
-  zone         = var.zone
+# resource "google_compute_instance" "controlplane" {
+#   name         = "k8s-controlplane"
+#   machine_type = "e2-medium"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20251023"
-      size  = 50
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20251023"
+#       size  = 50
+#     }
+#   }
 
-  network_interface {
-    subnetwork = google_compute_subnetwork.k8s_subnet.id
-  }
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.k8s_subnet.id
+#   }
 
-  tags = ["k8s-node", "controlplane", "iap-ssh"]
-}
+#   tags = ["k8s-node", "controlplane", "iap-ssh"]
+# }
 
-resource "google_compute_instance" "worker" {
-  name         = "k8s-worker"
-  machine_type = "e2-medium"
-  zone         = var.zone
+# resource "google_compute_instance" "worker" {
+#   name         = "k8s-worker"
+#   machine_type = "e2-medium"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20251023"
-      size  = 50
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20251023"
+#       size  = 50
+#     }
+#   }
 
-  network_interface {
-    subnetwork = google_compute_subnetwork.k8s_subnet.id
-  }
+#   network_interface {
+#     subnetwork = google_compute_subnetwork.k8s_subnet.id
+#   }
 
-  tags = ["k8s-node", "worker", "iap-ssh"]
-}
+#   tags = ["k8s-node", "worker", "iap-ssh"]
+# }
--- terraform/router.tf
+++ terraform/router.tf
@@ -1,13 +1,13 @@
-resource "google_compute_router" "k8s_router" {
-  name    = "k8s-router"
-  network = google_compute_network.k8s_vpc.name
-  region  = "europe-west8"
-}
+# resource "google_compute_router" "k8s_router" {
+#   name    = "k8s-router"
+#   network = google_compute_network.k8s_vpc.name
+#   region  = "europe-west8"
+# }
 
-resource "google_compute_router_nat" "k8s_nat" {
-  name                               = "k8s-nat"
-  router                             = google_compute_router.k8s_router.name
-  region                             = google_compute_router.k8s_router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
+# resource "google_compute_router_nat" "k8s_nat" {
+#   name                               = "k8s-nat"
+#   router                             = google_compute_router.k8s_router.name
+#   region                             = google_compute_router.k8s_router.region
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+# }
